766e2a970bd6e22293fed6c1bdf6d4409957e5c2
Damber Singh <12190003.gcit@rub.edu.bt>
2023-07-12 16:19:43 +0600

    updates

--- analysis/myapp/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.signin, name="signin" ),
-    path('register/', views.register, name="register" ),
-    path('home', views.home, name="home" ),
-    path('logout', views.log_out, name='logout'),
-]
