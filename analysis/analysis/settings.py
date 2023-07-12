bfaba59e8c8e271e38bc2b0a71b371a14bbaad28
Damber Singh Subedi <89856647+damtz@users.noreply.github.com>
2023-07-12 09:29:37 +0600

    Update settings.py

--- analysis/analysis/settings.py
+++ analysis/analysis/settings.py
@@ -43,7 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework',
+    # 'rest_framework',
     'myapp',
 ]
 # CORS_ORIGIN_ALLOW_ALL = True
